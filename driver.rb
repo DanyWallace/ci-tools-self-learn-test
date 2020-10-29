bd4730450d2c1bab73c102809619ea10928dd018
teeigeryuh <2091853+teeigeryuh@users.noreply.github.com>
2020-10-28 18:20:07 -0700

    Update driver.rb

--- driver.rb
+++ driver.rb
@@ -1,5 +1,15 @@
 load 'utils.rb'
 
-10.times do
-  puts numConsonantGen 10
+load 'utils.rb'
+
+result = ''
+20.times do
+  result << numConsonantGen 12
+end
+
+num_file = File.new("nums.txt", "w+")
+if num_file
+   num_file.syswrite(result)
+else
+   puts "Couldn't work on this file"
 end
